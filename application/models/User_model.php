098280384754a36b81b33f93644c500cee931771
awapa9941 <jametakun48@gmail.com>
2024-10-18 23:39:28 +0800

    first commit

---
+++ application/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+class User_model1 extends CI_Model
+{
+    protected$_table = 'user';
+    protected $primary = 'id';
+
+public function getAll()
+{
+    return $this->db->where('is_active',1)->get($this->_table)->result();
+}
+}
---
+++ application/views/user/index.php
@@ -0,0 +1,11 @@
+<main>
+    <div class="container-fluid">
+        <h1 class"mt-4"></h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrump-item"><a href="<?php echo site_url("user") ?>">User</li>
+            <li class="breadcrump-item active"><?php echo $title ?></li>
+        </ol>
+        <div class="card mb-4">
+             
+    </div>
+</main>
